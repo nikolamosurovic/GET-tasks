bf5a2ef443ef455307556db3c00826e4a91376c3
nikola <nikolamosurovic1@gmail.com>
2024-12-30 21:09:55 +0100

    add sns & lambda

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,13 @@ output "ec2_private_ip" {
   value = aws_instance.test_ec2.private_ip
   description = "Private IP address of the EC2 instance"
 }
+
+output "sns_topic_arn" {
+  value       = aws_sns_topic.daily_email_topic.arn
+  description = "ARN of the SNS topic"
+}
+
+output "lambda_function_name" {
+  value       = aws_lambda_function.send_daily_email.function_name
+  description = "Name of the Lambda function"
+}
