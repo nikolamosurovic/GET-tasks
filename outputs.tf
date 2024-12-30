35204ec3133bad9502248b1ac76a40ff5bfa023d
nikola <nikolamosurovic1@gmail.com>
2024-12-30 13:56:19 +0100

    add description ip private

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "ec2_public_ip" {
 output "rds_endpoint" {
   value = aws_db_instance.test_rds.address
 }
+
+output "ec2_private_ip" {
+  value = aws_instance.test_ec2.private_ip
+  description = "Private IP address of the EC2 instance"
+}
